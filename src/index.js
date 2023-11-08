6289feb4b4d4fdc7150570274b4fe889e56b74c4
SVTroya <my.svtroy@gmail.com>
2023-11-08 11:50:42 +0200

    Commit

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import "simplelightbox/dist/simple-lightbox.min.css"
 
 const URL_PIXABAY = "https://pixabay.com/api/"
 const KEY_PIXABAY = "40532250-7000c5ca3f9409cc384b0640a"
+const IMG_PER_PAGE = 40
 const ERR = {
   EMPTY: 0,
   NOT_FOUND: 1,
   END: 2,
+}
+const NOTIFICATION = {
+  END: 0,
+  FOUND: 1
 }
 
 const els = {
@@ -16,44 +21,38 @@ const els = {
   inputSearchEl: document.querySelector("input.input-search"),
   btnSearchEl: document.querySelector("button.btn-search"),
   galleryEl: document.querySelector("div.gallery"),
+  btnLoadMoreEl: null,
 }
 
-const gallery = new SimpleLightbox('.gallery a')
+let gallery = null
+let page = 1
+let query = ""
 
-/*els.galleryEl.onclick = (event) => {
-  event.preventDefault()
-}*/
+els.formEl.onsubmit = onSubmit
 
-els.formEl.onsubmit = async (event) => {
+async function onSubmit(event) {
   event.preventDefault()
+  setToDefault()
   const inputValue = event.target.elements.searchQuery.value
   if (!inputValue) {
     showError(ERR.EMPTY)
     return
   }
-  const query = inputValue.split(" ").join("+")
-  try {
-    const galleryItems = await fetchImages(query)
-    if (!galleryItems.hits.length) {
-      showError(ERR.NOT_FOUND)
-      return
-    }
-    renderGallery(galleryItems.hits)
-    gallery.refresh()
-    els.formEl.reset()
-  } catch (error) {
-    console.log(error)
-    showError(ERR.NOT_FOUND)
-  }
+  query = inputValue.split(" ").join("+")
+  await getImages()
+  gallery = new SimpleLightbox(".gallery a")
+  els.formEl.reset()
 }
 
-async function fetchImages(query) {
+async function fetchImages() {
   const params = new URLSearchParams({
     key: KEY_PIXABAY,
     q: query,
     image_type: "photo",
     orientation: "horizontal",
-    safesearch: "true",
+    safesearch: true,
+    per_page: IMG_PER_PAGE,
+    page: page,
   })
 
   const response = await axios.get(`${URL_PIXABAY}?${params}`)
@@ -77,10 +76,23 @@ function showError(errorCode) {
   Notiflix.Notify.failure(message)
 }
 
-function renderGallery(galleryItems) {
+function showNotification(notificationCode, totalHits) {
+  let message = ""
+  switch (notificationCode) {
+    case 0:
+      message = "You've reached the end of search results 😉."
+      break
+    case 1:
+      message = `Hooray! We found ${totalHits} images.`
+      break
+  }
+
+  Notiflix.Notify.success(message)
+}
+
+function renderGallery(galleryItems, hasMore) {
   const markup = galleryItems.map(image =>
-    `<div class="photo-card">
-    <a class="gallery-item" href="${image.largeImageURL}">
+    `<a class="gallery-item" href="${image.largeImageURL}">
       <img class="photo-card" src="${image.webformatURL}" alt="${image.tags}"  loading="lazy" />
       <div class="info">
         <p class="info-item">
@@ -100,9 +112,65 @@ function renderGallery(galleryItems) {
           ${image.downloads}
         </p>
       </div>
-    </a>
-  </div>`)
+    </a>`)
     .join("")
 
   els.galleryEl.insertAdjacentHTML("beforeend", markup)
+
+  if(hasMore) {
+    addLoadMoreBtn()
+  }
+  else {
+    showNotification(NOTIFICATION.END)
+  }
+  smoothScroll()
+}
+
+function setToDefault() {
+  els.galleryEl.innerHTML = ""
+  query = ""
+  page = 1
+  if (els.btnLoadMoreEl) {
+    els.btnLoadMoreEl.remove()
+  }
+}
+
+async function onLoadMoreClicked() {
+  els.btnLoadMoreEl.remove()
+  page++
+  await getImages()
+  gallery.refresh()
+}
+
+function addLoadMoreBtn() {
+  els.btnLoadMoreEl = document.createElement("button")
+  els.btnLoadMoreEl.className = "gallery-item"
+  els.btnLoadMoreEl.textContent = "Load more"
+  els.galleryEl.append(els.btnLoadMoreEl)
+  els.btnLoadMoreEl.onclick = onLoadMoreClicked
+}
+
+async function getImages() {
+  try {
+    const galleryItems = await fetchImages()
+    if (!galleryItems.hits.length) {
+      showError(ERR.NOT_FOUND)
+      return
+    }
+    if (page === 1) {
+      showNotification(NOTIFICATION.FOUND, galleryItems.totalHits)
+    }
+    const hasMore = (galleryItems.totalHits - IMG_PER_PAGE * page) > 0
+    renderGallery(galleryItems.hits, hasMore)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function smoothScroll() {
+  const { height: cardHeight } = els.galleryEl.firstElementChild.getBoundingClientRect()
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth"
+  })
 }
