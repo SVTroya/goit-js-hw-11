71d692f335677fa1a8272e02bbbdca7124eea085
SVTroya <my.svtroy@gmail.com>
2023-11-09 12:28:04 +0200

    Commit

--- src/js/index.js
+++ src/js/index.js
@@ -28,10 +28,10 @@ function clearGallery() {
 }
 
 document.addEventListener("scroll", throttle(() => {
-  if (window.scrollY < 900) {
+  if (window.scrollY < 900 && btnScrollUpEl.style.transform !== "scale(0)") {
     btnScrollUpEl.style.transform = "scale(0)"
   }
-  if (window.scrollY > 900) {
+  if (window.scrollY > 900 && btnScrollUpEl.style.transform !== "scale(1)") {
     btnScrollUpEl.style.transform = "scale(1)"
   }
 }, 200))
